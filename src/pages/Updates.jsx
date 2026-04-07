d4823a4104b324f67a88c7d266fca6f79fcb7eb7
vikramlota <viccckkylota@gmail.com>
2026-04-07 22:42:52 +0530

    Update Updates.jsx

--- src/pages/Updates.jsx
+++ src/pages/Updates.jsx
@@ -1,14 +1,10 @@
 import React from 'react';
 import Updates from '../components/Updates';
-import { Helmet } from 'react-helmet';
+
 const UpdatesPage = () => {
   return (
 <>
 
-    <Helmet>
-      <title>Latest Govt Exam Notifications & Updates | Samarth Academy</title>
-      <meta name="description" content="Stay updated with the latest notifications, admit cards, and exam dates for SSC, Banking, Punjab Police, and State exams." />
-    </Helmet>
  < Updates/>  
    
 </>
